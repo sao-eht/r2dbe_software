541f103187666a5ab9add05418a5c66dbff3c169
anyoung <ayoung.astro.ru@gmail.com>
2018-08-29 17:03:03 +0200

    [lib/mandc] Implement basic CheckingDevoce methods

--- lib/mandc/primitives/devices.py
+++ lib/mandc/primitives/devices.py
@@ -1,6 +1,33 @@
 import os
 
+from ..utils import TerminalMessage
+
+class CheckResult(object):
+
+	@property
+	def description(self):
+		return self._description
+
+	@property
+	def result(self):
+		return self._result
+
+	def __init__(self, desc, result):
+		self._description = desc
+		self._result = result
+
+	def __str__(self):
+		if self.result:
+			return TerminalMessage.okay(self.description)
+
+		return TerminalMessage.fail(self.description)
+
 class CheckingDevice(object):
+
+	CHK = None
+	CHK_EQ = 1
+	CHK_LT = 2
+	CHK_GT = 3
 
 	@classmethod
 	def available_check(cls, identifier):
@@ -10,8 +37,36 @@ class CheckingDevice(object):
 	def __init__(self, host):
 		self.host = host
 
+	def do_check(self, msg, func, assrt=True, ctype=None):
+		if ctype == self.CHK:
+			# func() returns True on PASS, False on FAIL
+			return CheckResult("{name}: {desc}".format(name=self.host, desc=msg),
+			  func())
+
+		if ctype == self.CHK_EQ:
+			# PASS on func() == assrt, FAIL otherwise
+			return CheckResult("{name}: {desc}".format(name=self.host, desc=msg),
+			  func() == assrt)
+
+		if ctype == self.CHK_LT:
+			# PASS on func() < assrt, FAIL otherwise
+			return CheckResult("{name}: {desc}".format(name=self.host, desc=msg),
+			  func() < assrt)
+
+		if ctype == self.CHK_GT:
+			# PASS on func() > assrt, FAIL otherwise
+			return CheckResult("{name}: {desc}".format(name=self.host, desc=msg),
+			  func() > assrt)
+
+	def do_checklist(self, checklist):
+		resultlist = []
+		for msg, func, assrt, ctype in checklist:
+			resultlist.append(self.do_check(msg, func, assrt=assrt, ctype=ctype))
+
+		return resultlist
+
 	def pre_config_checks(self):
-		pass
+		return self.do_checklist([])
 
 	def post_config_checks(self):
-		pass
+		return self.do_checklist([])
