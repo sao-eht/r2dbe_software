3d119682d76907f1cd1b8578a398934fcbd4ce77
anyoung <ayoung.astro.ru@gmail.com>
2019-01-09 09:53:30 +0100

    [lib] Add BDC to backend __repr__

--- lib/mandc/hierarchy.py
+++ lib/mandc/hierarchy.py
@@ -32,7 +32,7 @@ class Backend(CheckingDevice):
 		  r2dbe=self.r2dbe.host, mark6=self.mark6.host))
 
 	def __repr__(self):
-		repr_str = "{name}:%r>>%r" % (self.r2dbe,self.mark6.host)
+		repr_str = "{name}:|%r>>%r>>%r|" % (self.bdc,self.r2dbe,self.mark6)
 		return repr_str.format(name=self.name)
 
 	def setup(self):
