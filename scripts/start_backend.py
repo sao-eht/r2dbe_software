22c6c1408b78eddcec7288c4299ce08634a4c42d
anyoung <andre.young@cfa.harvard.edu>
2019-01-10 16:10:50 +0000

    [scripts/start_backend] Use utils logging conf

--- scripts/start_backend.py
+++ scripts/start_backend.py
@@ -5,59 +5,10 @@ import os.path
 import sys
 
 from mandc import Station
-from mandc.utils import TerminalMessenger
+from mandc.utils import TerminalMessenger, configure_logging
 
 _default_log_basename = os.path.extsep.join([os.path.basename(os.path.splitext(__file__)[0]), "log"])
 _default_log = os.path.sep.join([os.path.expanduser("~"), "log",_default_log_basename])
-
-def _configure_logging(logfilename=None, verbose=None, stdout_logger=True):
-	# Set up root logger
-	logger = logging.getLogger()
-	logger.setLevel(logging.INFO)
-
-	all_handlers = []
-
-	# Optionally log to stdout
-	stdout_handler = None
-	if stdout_logger:
-		stdout_handler = logging.StreamHandler(sys.stdout)
-		all_handlers.append(stdout_handler)
-	# Optionally log to file
-	file_handler = None
-	if logfilename:
-		file_handler = logging.FileHandler(logfilename, mode="a")
-		all_handlers.append(file_handler)
-	# Add handlers
-	for handler in all_handlers:
-		logger.addHandler(handler)
-
-	# Silence all katcp messages, except CRITICAL
-	katcp_logger = logging.getLogger('katcp')
-	katcp_logger.setLevel(logging.CRITICAL)
-
-	# If verbose, set level to DEBUG on file, or stdout if no logging to file
-	if verbose:
-		# First set DEBUG on root logger
-		logger.setLevel(logging.DEBUG)
-		# Then revert to INFO on 0th handler (i.e. stdout)
-		if stdout_handler is not None:
-			stdout_handler.setLevel(logging.INFO)
-		# Finally DEBUG again on 1th handler (file if it exists, otherwise stdout again)
-		if file_handler is not None:
-			file_handler.setLevel(logging.DEBUG)
-
-	# Create and set formatters
-	formatter = logging.Formatter('%(name)-30s: %(asctime)s : %(levelname)-8s %(message)s')
-	for handler in all_handlers:
-		handler.setFormatter(formatter)
-
-	# Initial log messages
-	logger.info("Started logging in {filename}".format(filename=__file__))
-	if logfilename:
-		logger.info("Log file is '{log}'".format(log=logfilename))
-
-	# Return root logger
-	return logger
 
 if __name__ == "__main__":
 	import argparse
@@ -72,7 +23,7 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	# Configure logging
-	logger = _configure_logging(logfilename=args.log, verbose=args.verbose,
+	logger = configure_logging(logfilename=args.log, verbose=args.verbose,
 	  stdout_logger=False)
 
 	# Configure UI
